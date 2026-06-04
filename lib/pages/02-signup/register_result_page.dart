9484094a0ed97964724d08af708e87d91b377891
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-06-04 21:12:20 +0800

    Updated added some listView for webview mobile

--- lib/pages/02-signup/register_result_page.dart
+++ lib/pages/02-signup/register_result_page.dart
@@ -9,44 +9,48 @@ class RegisterResultPage extends StatelessWidget {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 300),
-              Icon(
-                Icons.check_circle_rounded,
-                size: 97.5,
-                color: theme.colorScheme.primary,
-                semanticLabel: 'checkmark logo',
-              ),
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 300),
+                  Icon(
+                    Icons.check_circle_rounded,
+                    size: 97.5,
+                    color: theme.colorScheme.primary,
+                    semanticLabel: 'checkmark logo',
+                  ),
 
-              Text(
-                'Account created successfully !',
-                textAlign: TextAlign.center,
-                style: theme.textTheme.headlineMedium,
-              ),
-              Text(
-                'You’re all set! Enjoy a faster way to pay your bills and submit service requests anytime, anywhere.',
-                textAlign: TextAlign.center,
-                style: theme.textTheme.labelSmall,
-              ),
+                  Text(
+                    'Account created successfully !',
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.headlineMedium,
+                  ),
+                  Text(
+                    'You’re all set! Enjoy a faster way to pay your bills and submit service requests anytime, anywhere.',
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.labelSmall,
+                  ),
 
-              const SizedBox(height: 300),
-              PrimaryButton(
-                label: 'Login to get started',
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    '/login',
-                    (route) => false,
-                  );
-                },
+                  const SizedBox(height: 300),
+                  PrimaryButton(
+                    label: 'Login to get started',
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/login',
+                        (route) => false,
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
